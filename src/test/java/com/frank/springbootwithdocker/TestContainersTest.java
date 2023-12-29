cfaf7436e21dd98cd0d41a0bdc0c7c36296a8b72
FrankHuang <frank.huang@kooppi.com>
2023-12-29 15:45:52 +0800

    refactor: refactor with abstract class

--- src/test/java/com/frank/springbootwithdocker/TestContainersTest.java
+++ src/test/java/com/frank/springbootwithdocker/TestContainersTest.java
@@ -1,53 +1,14 @@
 package com.frank.springbootwithdocker;
 
-import org.flywaydb.core.Flyway;
 import org.junit.jupiter.api.Test;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@Testcontainers
-class TestContainersTest {
-
-    @Container
-    private static final PostgreSQLContainer<?> postgreSQLContainer =
-            new PostgreSQLContainer<>("postgres:latest") // 創建 test 專用 db 容器
-            .withDatabaseName("frank-dao-unit-test")
-            .withUsername("frank")
-            .withPassword("password");
+class TestContainersTest extends BaseTestContainers {
 
     @Test
     void canStartPostgresDB() {
         assertThat(postgreSQLContainer.isCreated()).isTrue();
         assertThat(postgreSQLContainer.isRunning()).isTrue();
-    }
-
-    /**
-     * 使用 spring 提供的 DynamicPropertyRegistry 進行 test 中也能設定配置
-     * @param registry
-     */
-    @DynamicPropertySource
-    private static void registerDataSourceProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url", postgreSQLContainer::getJdbcUrl);
-        registry.add("spring.datasource.username", postgreSQLContainer::getUsername);
-        registry.add("spring.datasource.password", postgreSQLContainer::getPassword);
-    }
-
-    /**
-     * 讓 test container 可以運行 flyway 做 db_migration
-     */
-    @Test
-    void canApplyDbMigrateWithFlyway() {
-        Flyway flyway = Flyway.configure().dataSource(
-                postgreSQLContainer.getJdbcUrl(),
-                postgreSQLContainer.getUsername(),
-                postgreSQLContainer.getPassword()
-        ).load();
-        flyway.migrate();
-        System.out.println("==== canApplyDbMigrateWithFlyway finished ====");
     }
 }
