6e7b747f9eea704d42f7ed15550dbab2f7b9150d
FrankHuang <frank.huang@kooppi.com>
2023-12-29 10:04:33 +0800

    feat: use Faker

---
+++ src/main/java/com/frank/springbootwithdocker/Main.java
@@ -0,0 +1,35 @@
+package com.frank.springbootwithdocker;
+
+import com.frank.springbootwithdocker.entity.Customer;
+import com.frank.springbootwithdocker.repository.CustomerRepository;
+import com.github.javafaker.Faker;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Random;
+
+@SpringBootApplication
+public class Main implements CommandLineRunner {
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Faker faker = new Faker();
+        Random random = new Random();
+        String lastName = faker.name().lastName();
+        String firstName = faker.name().firstName();
+        String name = firstName + " " + lastName;
+        String email = firstName + "." + lastName + "@example.com";
+        int age = random.nextInt(16, 99);
+        Customer customer = Customer.builder().name(name).email(email).age(age).build();
+        customerRepository.save(customer);
+    }
+}
--- src/main/java/com/frank/springbootwithdocker/entity/Customer.java
+++ src/main/java/com/frank/springbootwithdocker/entity/Customer.java
@@ -20,7 +20,7 @@ public class Customer {
     @Id
     @SequenceGenerator(name = "customer_id_seq", sequenceName = "customer_id_seq", allocationSize = 1)
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "customer_id_seq")
-    private Integer id;
+    private Long id;
 
     @Column(name = "name", nullable = false)
     private String name;
